2888542804e9707018937cb9348b5afa635a63b9
Slimaine BENSADOUN <slimaine.bensadoun-prestataire@labanquepostale.fr>
2018-08-07 14:49:45 +0200

    Adding High Availability Configuration

--- admin-server/src/main/java/com/sbn/admin/AdminApplicationStarter.java
+++ admin-server/src/main/java/com/sbn/admin/AdminApplicationStarter.java
@@ -3,18 +3,66 @@ package com.sbn.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import de.codecentric.boot.admin.server.domain.entities.InstanceRepository;
+import de.codecentric.boot.admin.server.notify.LoggingNotifier;
 
 @EnableAdminServer
 @SpringBootApplication
 @EnableDiscoveryClient
 public class AdminApplicationStarter {
 
+	
+    private final String adminContextPath;
+
+    public AdminApplicationStarter(AdminServerProperties adminServerProperties) {
+        this.adminContextPath = adminServerProperties.getContextPath();
+    }
+    
 	public static void main(String[] args) {
 		SpringApplication.run(AdminApplicationStarter.class, args);
 	}
 
+	
+	
+    @Bean
+    @Profile("insecure")
+    public SecurityWebFilterChain securityWebFilterChainPermitAll(ServerHttpSecurity http) {
+        return http.authorizeExchange().anyExchange().permitAll()//
+                   .and().csrf().disable()//
+                   .build();
+    }
+	
+    
+    @Bean
+    @Profile("secure")
+    public SecurityWebFilterChain securityWebFilterChainSecure(ServerHttpSecurity http) {
+        // @formatter:off
+        return http.authorizeExchange()
+                .pathMatchers(adminContextPath + "/assets/**").permitAll()
+                .pathMatchers(adminContextPath + "/login").permitAll()
+                .pathMatchers(adminContextPath + "/actuator/**").permitAll()
+                .anyExchange().authenticated()
+                .and()
+            .formLogin().loginPage(adminContextPath + "/login").and()
+            .logout().logoutUrl(adminContextPath + "/logout").and()
+            .httpBasic().and()
+            .csrf().disable()
+            .build();
+        // @formatter:on
+    }
+    
+    @Bean
+    public LoggingNotifier loggerNotifier(InstanceRepository repository) {
+        return new LoggingNotifier(repository);
+    }
+    
 	/*
 	 * @Configuration public static class SecurityPermitAllConfig extends
 	 * WebSecurityConfigurerAdapter {
--- first-business-service/src/main/java/com/sbn/firstbusiness/controller/FirstBusinessServiceController.java
+++ first-business-service/src/main/java/com/sbn/firstbusiness/controller/FirstBusinessServiceController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,6 +25,17 @@ public class FirstBusinessServiceController {
 	
 	@Autowired
 	SecondServiceClient secondServiceClient;
+	
+	
+	@Value("${welcome.message}")
+	private String welcomeMessage;
+
+	@GetMapping("/welcome")
+	public String retrieveWelcomeMessage() {
+		// Complex Method
+		return welcomeMessage;
+	}
+	
 
 	@RequestMapping(value = "/getProductByCategory/{category}", method = RequestMethod.GET)
 	public List<String> getProductByCategory(@PathVariable String category) {
