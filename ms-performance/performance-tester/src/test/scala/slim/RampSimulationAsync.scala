109b22496d8dfffb37eb4d0f300325833e2ade85
Slimaine BENSADOUN <slimaine.bensadoun-prestataire@labanquepostale.fr>
2018-10-30 10:05:40 +0100

    Dynamisation of performance script

--- ms-performance/performance-tester/src/test/scala/slim/RampSimulationAsync.scala
+++ ms-performance/performance-tester/src/test/scala/slim/RampSimulationAsync.scala
@@ -9,18 +9,24 @@ import io.gatling.jdbc.Predef._
 //Run command : mvn gatling:test -Dgatling.simulationClass=slim.RampSimulationAsync
 class RampSimulationAsync extends Simulation {
 
-	val httpProtocol = http
-		.baseURL("http://localhost:8091")
-		.inferHtmlResources()
-		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-		.acceptEncodingHeader("gzip, deflate")
-		.acceptLanguageHeader("fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3")
-		.userAgentHeader("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0")
+  val nbUsers = Integer.getInteger("users", 1)
+  val rampTime = java.lang.Long.getLong("ramp", 0L)
+  val nbRepeat = java.lang.Long.getLong("repeat", 0L)
+  
+  
+  val httpProtocol = http
+    .baseURL("http://localhost:8091")
+    .inferHtmlResources()
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    .acceptEncodingHeader("gzip, deflate")
+    .acceptLanguageHeader("fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3")
+    .userAgentHeader("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0")
 
-
-	val scn = scenario("RampSimulationAsync")
-		.exec(http("request_0")
-			.get("/waitTimeAsync3/300"))
-
-	setUp(scn.inject(rampUsers(1000) over(5 seconds))).protocols(httpProtocol)
+  val scn = scenario("RampSimulationAsync").exec(
+    repeat(30) {
+      exec(
+        http("request_0").get("/waitTimeAsync/300")
+        ).pause(1 second, 2 seconds)
+    })
+  setUp(scn.inject(rampUsers(1000) over (5 seconds))).protocols(httpProtocol)
 }
---
+++ ms-performance/performance-tester/src/test/scala/slim/RampSimulationDynamic.scala
@@ -0,0 +1,34 @@
+package slim
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+//Run command : mvn gatling:test -Dgatling.simulationClass=slim.RampSimulationDynamic -DbaseUrl=http://localhost:8091 -DmethodUrl=/waitTimeAsync/300 -DnbUsers=50 -DrampTime=5 -DnbRepeats=1
+class RampSimulationDynamic extends Simulation {
+
+  val baseUrl: String = System.getProperty("baseUrl")
+  val methodUrl: String = System.getProperty("methodUrl")
+  val nbUsers = Integer.getInteger("nbUsers", 1)
+  val rampTime = Integer.getInteger("rampTime", 1)
+  val nbRepeats: Int = System.getProperty("nbRepeats").toInt
+  
+  
+  val httpProtocol = http
+    .baseURL(baseUrl)
+    .inferHtmlResources()
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    .acceptEncodingHeader("gzip, deflate")
+    .acceptLanguageHeader("fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3")
+    .userAgentHeader("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0")
+
+  val scn = scenario("RampSimulationAsync").exec(
+    repeat(nbRepeats) {
+      exec(
+        http("request_0").get(methodUrl)
+        ).pause(1 second, 2 seconds)
+    })
+  setUp(scn.inject(rampUsers(nbUsers) over (rampTime seconds))).protocols(httpProtocol)
+}
